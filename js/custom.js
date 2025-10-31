040ae25bc0fbe6cc23cf18c3c294e0346bb43770
Siniša Paskulov <paskulov.sinisa@icloud.com>
2025-10-31 12:56:07 +0100

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/bible_with_leaves.jpg' },
+              // { src: 'images/bible_wind.jpg' },
+          ],
+          timer: false,
+          delay: 15e3,
+          animation: 'kenburns',
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+  
+      scrollToDiv(elWrapped);
+      return false;
+  
+      function scrollToDiv(element){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
